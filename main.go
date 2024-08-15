13b70dcb7a93d3d12c03d6a9f29e1316da51aafb
Tharindu Munaweera <tharindumunaweera94@gmail.com>
2024-08-15 17:14:39 +0000

    data types

--- main.go
+++ main.go
@@ -6,9 +6,18 @@ func main() {
 	var conferenceName = "Go conference"
 	const conferenceTickets = 50
 	var remainingTickets = 50
+	fmt.Printf("conferenceName is %T and conferenceTikets is %T and remainingTickets is %T. \n", conferenceName, conferenceTickets, remainingTickets)
 	fmt.Println("welocme to out", conferenceName, "conference application")
-	fmt.Println("we have total of",  conferenceTickets, "tikets and", remainingTickets)
+	fmt.Printf("we have total of %v tikets and %v.\n", conferenceTickets, remainingTickets)
 	fmt.Println("get your tickets from here");
 
+	var userName string
+	var userTickets int
 
-} 
+	userName = "tharindu"
+	userTickets = 10
+
+	fmt.Printf("user name %v and tickets count %v.\n", userName, userTickets)
+
+
+}  
