bc27dab03f26740457e9fc16bee56891d28bddf1
Tharindu Munaweera <tharindumunaweera94@gmail.com>
2024-08-15 02:37:28 +0000

    variables and constants

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var conferenceName = "Go conference"
+	const conferenceTickets = 50
+	var remainingTickets = 50
+	fmt.Println("welocme to out", conferenceName, "conference application")
+	fmt.Println("we have total of",  conferenceTickets, "tikets and", remainingTickets)
+	fmt.Println("get your tickets from here");
+
+
+} 
