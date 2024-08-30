effbd7cae9ea922ab3df96ac8ebeb27fe63867c2
Tharindu Munaweera <tharindumunaweera94@gmail.com>
2024-08-30 16:25:24 +0000

    for loop

--- main.go
+++ main.go
@@ -12,33 +12,35 @@ func main() {
 	fmt.Printf("we have total of %v tikets and %v.\n", conferenceTickets, remainingTickets)
 	fmt.Println("get your tickets from here");
 
-	var userName string
-	var lastName string
-	var email string
-	var userTickets int
-
-	// ask for the user input
-	fmt.Print("enter your first name: ")
-	fmt.Scan(&userName)
-
-	fmt.Print("enter your last name: ")
-	fmt.Scan(&lastName)
-
-	fmt.Print("enter your email: ")
-	fmt.Scan(&email)
-
-	fmt.Print("how many tickets do u want : ")
-	fmt.Scan(&userTickets)
-
-	remainingTickets = remainingTickets - uint(userTickets)
-	bookings = append(bookings, userName + " " + lastName)
-
-	fmt.Printf("The whole slice: %v\n", bookings)
-	fmt.Printf("The first value : %v\n", bookings[0])
-	fmt.Printf("slice type: %T\n", bookings)
-	fmt.Printf("slice length: %v\n", len(bookings))
-
-	fmt.Printf("Thank your for %v %v for book %v tickets and receipt will be deliver %v address\n", userName, lastName, userTickets, email)
-	fmt.Printf("%v tickets are remaining\n", remainingTickets)
+	for {
+		var userName string
+		var lastName string
+		var email string
+		var userTickets int
+	
+		// ask for the user input
+		fmt.Print("enter your first name: ")
+		fmt.Scan(&userName)
+	
+		fmt.Print("enter your last name: ")
+		fmt.Scan(&lastName)
+	
+		fmt.Print("enter your email: ")
+		fmt.Scan(&email)
+	
+		fmt.Print("how many tickets do u want : ")
+		fmt.Scan(&userTickets)
+	
+		remainingTickets = remainingTickets - uint(userTickets)
+		bookings = append(bookings, userName + " " + lastName)
+	
+		fmt.Printf("The whole slice: %v\n", bookings)
+		fmt.Printf("The first value : %v\n", bookings[0])
+		fmt.Printf("slice type: %T\n", bookings)
+		fmt.Printf("slice length: %v\n", len(bookings))
+	
+		fmt.Printf("Thank your for %v %v for book %v tickets and receipt will be deliver %v address\n", userName, lastName, userTickets, email)
+		fmt.Printf("%v tickets are remaining\n", remainingTickets)
+	}
 
 }  
